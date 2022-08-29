b2f964c8140b7eb33a96338c619e4290c27eef02
Zanng <emerson_10_s@hotmail.com>
2022-08-28 21:04:04 -0300

    component sidebar

---
+++ src/components/Header/Header.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Header.css";
+
+import SearchIcon from "@material-ui/icons/Search";
+import HomeIcon from "@material-ui/icons/Home";
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationIcon from "@material-ui/icons/Notifications";
+
+import HeaderOptions from "../HeaderOptions/HeaderOptions";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/3536/3536505.png"
+          alt="Logo"
+        />
+
+        <div className="header_search">
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </div>
+
+      <div className="header__right">
+        <HeaderOptions Icon={HomeIcon} title="Home" />
+        <HeaderOptions Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOptions Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOptions Icon={ChatIcon} title="Messaging" />
+        <HeaderOptions Icon={NotificationIcon} title="Notifications" />
+        <HeaderOptions avatar="https://avatars.githubusercontent.com/u/42453905?v=4" title="me" />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import './Sidebar.css'
+
+import { Avatar } from '@material-ui/core'
+
+function Sidebar() {
+
+  const recentItem = (topic) => (
+    <div className='sidebar__recentItem'>
+      <span className='sidebar__hash'>#</span>
+      <p>{topic}</p>
+    </div>
+  )
+
+  return (
+    <div className='sidebar'>
+      <div className='sidebar__top'>
+        <img src='https://images.unsplash.com/photo-1661547663251-297d10e13b3b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1245&q=80' alt=''/>
+        <Avatar className='sidebar__avatar' src='https://avatars.githubusercontent.com/u/42453905?v=4'/>
+        <h2>Emerson Mendonça</h2>
+        <h4>emersonads2019@gmail.com</h4>
+      </div>
+
+      <div className='sidebar__stats'>
+        <div className='sidebar__stat'>
+          <p>Who viewed you</p>
+          <p className='sidebar__statNumber'>2,984</p>
+        </div>
+        <div className='sidebar__stat'>
+          <p>Views on post</p>
+          <p className='sidebar__statNumber'>8,845</p>
+        </div>
+      </div>
+
+      <div className='sidebar__bottom'>
+        <p>Recent</p>
+        {recentItem('reactjs')}
+        {recentItem('angular')}
+        {recentItem('softwareenginnering')}
+        {recentItem('reduxjs')}
+        {recentItem('nextjs')}
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
